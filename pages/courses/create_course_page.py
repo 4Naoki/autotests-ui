59fc7767206830acbc86ae2ba68aa0c2b283eddb
4Naoki <nikita.torkhov@gmail.com>
2026-09-22 20:34:09 +0400

    Наращивание тестовой базы и рефакторинг. Использование тестовых классов

--- pages/courses/create_course_page.py
+++ pages/courses/create_course_page.py
@@ -22,7 +22,7 @@ class CreateCoursePage(BasePage):
         self.create_course_toolbar = CreateCourseToolbarViewComponent(page)
 
         self.create_exercise_form = CreateCourseExerciseFormComponent(page)
-        self.create_exercise_toolbar = CreateCourseExercisesToolbarViewComponent(page)
+        self.create_course_exercise_toolbar = CreateCourseExercisesToolbarViewComponent(page)
 
     def check_visible_exercises_empty_view(self):
         self.exercises_empty_view.check_visible(
--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -4,13 +4,16 @@ from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
 
 
-@pytest.mark.registration
 @pytest.mark.regression
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+@pytest.mark.registration
+class TestRegistration:
+    def test_successful_registration(self, dashboard_page: DashboardPage, registration_page: RegistrationPage):
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.registration_form.fill(
+            email="user.name@gmail.com",
+            username="username",
+            password="password"
+        )
+        registration_page.click_registration_button()
 
-    registration_page.registration_form.fill('user@gmail.com', 'username', 'password')
-    registration_page.registration_form.check_visible('user@gmail.com', 'username', 'password')
-
-    registration_page.click_registration_button()
-    dashboard_page.dashboard_toolbar_view.check_visible()
+        dashboard_page.dashboard_toolbar_view.check_visible()
