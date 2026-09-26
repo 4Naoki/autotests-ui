aaee20220fcc1afec44c9876cd68fb56a6bdb53c
4Naoki <nikita.torkhov@gmail.com>
2026-09-26 19:49:31 +0400

    Added .gitignore

--- components/base_component.py
+++ components/base_component.py
@@ -1,5 +1,6 @@
 from typing import Pattern
 
+import allure
 from playwright.sync_api import Page, expect
 
 from tools.logger import get_logger
@@ -13,8 +14,10 @@ class BaseComponent:
 
     def check_current_url(self, expected_url: Pattern[str]):
         step = f'Checking current url: {expected_url}'
-        logger.info(step)
-        expect(self.page).to_have_url(expected_url)
+
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
 
     def refresh_page(self):
         self.page.reload()
